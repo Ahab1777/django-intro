530896a3d2b2189fab92e413806a91e2dc5489c0
Leonardo de Pinho <pinholeonardo1777@gmail.com>
2025-06-12 11:47:09 -0300

    part1 done

---
+++ mysite/main/urls.py
@@ -0,0 +1,10 @@
+#this is where we define the paths to our webpages
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('v1/', views.v1, name='v1'),
+
+]
